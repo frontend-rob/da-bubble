c273a9ecaa9cc85dd6800cf21e0620adba5992a9
Alexander Hörst <hoerst.alexander@gmail.com>
2025-04-30 19:52:38 +0200

    creating the basic structure of the menu

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [MainMenuComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
--- src/app/landing-page/main-menu/main-menu.component.ts
+++ src/app/landing-page/main-menu/main-menu.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
 export class MainMenuComponent {
+  showChannelList = true;
+  showUserList = true;
+  users:any;
+  channels:any;
 
+  newChannel() {
+    // Logic to create a new channel
+  }
+
+  openChannelList() {
+    // Logic to open the channel list
+  }
+
+  newMessages() {
+    // Logic to open new messages
+  }
+
+  toggleNav() {
+
+  }
 }
