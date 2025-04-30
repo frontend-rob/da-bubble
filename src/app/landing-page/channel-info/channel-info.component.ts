0354806fd12da77d25eb8b5c0f95dd047614af34
R.G. <frontend-rob@proton.me>
2025-04-30 17:57:05 +0200

    feat(landing-page): add basic components

---
+++ src/app/landing-page/channel-info/channel-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-info',
+  imports: [],
+  templateUrl: './channel-info.component.html',
+  styleUrl: './channel-info.component.scss'
+})
+export class ChannelInfoComponent {
+
+}
