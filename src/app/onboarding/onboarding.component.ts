6d3fa06078c398c890602661d9edf4d21cc224ab
R.G. <frontend-rob@proton.me>
2025-04-30 20:31:53 +0200

    feat(onboarding): add login screen

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-onboarding',
-  imports: [],
-  templateUrl: './onboarding.component.html',
-  styleUrl: './onboarding.component.scss'
+    selector: 'app-onboarding',
+    imports: [RouterLink],
+    templateUrl: './onboarding.component.html',
+    styleUrl: './onboarding.component.scss'
 })
 export class OnboardingComponent {
 
