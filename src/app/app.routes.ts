c13fcaf0823bf7829f7aaa6082d8f3f0deeddcc4
Schmax <main.tinwork784@passmail.net>
2025-04-30 17:22:45 +0200

    authgurad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
+import { redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['logon']);
+const redirectLoggedInToWorkspace = () => redirectLoggedInTo(['workspace']);
+
+export const routes: Routes = [
+    {
+        path: '',
+        //component: MainComponent,
+        //canActivate: [AuthGuard],
+        data: {authGuardPipe: redirectUnauthorizedToLogin},
+        children: [
+            {
+                //path: '',
+                //component: 
+            }
+        ],
+    },
+    {
+        path: 'logon',
+        //component: LoginComponent,
+        data: {authGuardPipe: redirectLoggedInToWorkspace},
+        children: [
+            {
+                //path: '',
+                //component: SignInComponent
+            },
+        ],
+    },
+
+];
