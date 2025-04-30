52e4244f69510d2e1266cd6e3bcb9517f307ef6d
R.G. <frontend-rob@proton.me>
2025-04-30 17:34:04 +0200

    add basic structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,17 +8,19 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble-826b4", 
-      appId: "1:943978163562:web:50493f4ae483fefde6c913", 
-      databaseURL: "https://dabubble-826b4-default-rtdb.europe-west1.firebasedatabase.app", 
-      storageBucket: "dabubble-826b4.firebasestorage.app", 
-      apiKey: "AIzaSyAOcV3fihl-DAYMVXSbkxHq2E5djll5mfs", 
-      authDomain: "dabubble-826b4.firebaseapp.com", 
-      messagingSenderId: "943978163562" 
-    })), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore()), 
+    providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+        projectId: "dabubble-826b4",
+        appId: "1:943978163562:web:50493f4ae483fefde6c913",
+        databaseURL: "https://dabubble-826b4-default-rtdb.europe-west1.firebasedatabase.app",
+        storageBucket: "dabubble-826b4.firebasestorage.app",
+        apiKey: "AIzaSyAOcV3fihl-DAYMVXSbkxHq2E5djll5mfs",
+        authDomain: "dabubble-826b4.firebaseapp.com",
+        messagingSenderId: "943978163562"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase()
-  )]
+    )]
 };
