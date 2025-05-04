586edd94c146276bbe657af908ca8046259bf0c1
Seb887 <seb.dev@posteo.de>
2025-05-04 13:01:36 +0200

    create message input field component

--- src/app/landing-page/chat/chat.component.ts
+++ src/app/landing-page/chat/chat.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageInputFieldComponent } from '../../shared/message-input-field/message-input-field.component';
 
 @Component({
   selector: 'app-chat',
-  imports: [CommonModule],
+  imports: [CommonModule, MessageInputFieldComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
---
+++ src/app/shared/message-input-field/message-input-field.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessageInputFieldComponent } from './message-input-field.component';
+
+describe('MessageInputFieldComponent', () => {
+  let component: MessageInputFieldComponent;
+  let fixture: ComponentFixture<MessageInputFieldComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MessageInputFieldComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MessageInputFieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
