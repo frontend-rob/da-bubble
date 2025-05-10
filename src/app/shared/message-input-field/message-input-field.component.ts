ed88512679379fe91caae19dd421662b3910eede
schmax <majorsoldier603@proton.me>
2025-05-10 19:21:05 +0200

    add chat service

--- src/app/shared/message-input-field/message-input-field.component.ts
+++ src/app/shared/message-input-field/message-input-field.component.ts
@@ -1,35 +1,85 @@
-import {CommonModule} from '@angular/common';
-import {Component, Input} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChatService } from '../../services/chat.service';
+import { Channel } from '../../interfaces/channel.interface';
+import { Timestamp } from 'firebase/firestore';
 
 @Component({
     selector: 'app-message-input-field',
+    standalone: true,
     imports: [CommonModule, FormsModule],
     templateUrl: './message-input-field.component.html',
-    styleUrl: './message-input-field.component.scss',
+    styleUrls: ['./message-input-field.component.scss'],
 })
-export class MessageInputFieldComponent {
-    @Input() placeholderText!: string;
+export class MessageInputFieldComponent implements OnInit  {
+    @Input() selectedChannel: Channel | undefined;
+    @Input() channels$: any;
+    @Input() placeholderText = 'Type a message...';
+    @Output() send = new EventEmitter<string>();
 
     userTagModalIsOpen = false;
     emojiModalIsOpen = false;
     messageInputData = '';
+    chatService = inject(ChatService);
+
+    ngOnInit(): void {
+
+        this.channels$ = this.chatService.getChannels();
+
+        this.channels$?.subscribe(async (channels: Channel[]) => {
+            if (channels.length === 0) {
+                // Noch keine Channels vorhanden, daher Default-Channel erstellen
+                const defaultChannel: Channel = {
+                    channelId: '', // Firestore setzt die ID automatisch
+                    type: 'default',
+                    channelName: 'Entwicklerteam',
+                    channelDescription: 'Default Channel',
+                    createdBy: 'system',
+                    channelMembers: [],
+                    createdAt: Timestamp.now(),
+                    updatedAt: Timestamp.now(),
+                };
+                try {
+                    await this.chatService.createChannel(defaultChannel);
+                    // Channels erneut laden, damit der neue Channel sichtbar wird
+                    this.channels$ = this.chatService.getChannels();
+                } catch (error) {
+                    console.error('Fehler beim Anlegen des Default-Channels:', error);
+                }
+            } else if (!this.selectedChannel) {
+                this.selectedChannel = channels[0];
+            }
+        });
+    }
 
     toggleEmojiModal() {
-        if (!this.emojiModalIsOpen) {
-            this.emojiModalIsOpen = true;
-        } else {
-            this.emojiModalIsOpen = false;
-        }
+        this.emojiModalIsOpen = !this.emojiModalIsOpen;
     }
 
     toggleUserTagModal() {
-        if (!this.userTagModalIsOpen) {
-            this.userTagModalIsOpen = true;
+        this.userTagModalIsOpen = !this.userTagModalIsOpen;
+        if (this.userTagModalIsOpen) {
             this.messageInputData = '@';
         } else {
-            this.userTagModalIsOpen = false;
             this.messageInputData = '';
+        }
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            this.trySendMessage();
+        }
+    }
+
+    trySendMessage() {
+        console.log('try send message');
+        const trimmedMessage = this.messageInputData.trim();
+        if (trimmedMessage.length > 0) {
+            console.log(this.send);
+            this.send.emit(trimmedMessage);
+            this.messageInputData = ''; // clear input after sending
         }
     }
 }
