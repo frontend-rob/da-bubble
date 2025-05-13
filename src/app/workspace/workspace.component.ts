fe5be8827b736b7dee3cb8efc587452ca0f69f5c
R.G. <frontend-rob@proton.me>
2025-05-13 15:55:16 +0200

    feat(auth): improve status updates on logIn/logOut
    update includes:
    - after successful logIn, the user's status is updated to 'online' in Firestore.
    - after logOut, the user's status is updated to 'offline' in Firestore.
    
    This ensures correct user status reflection in the app.

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,19 +1,33 @@
-import {Component} from '@angular/core';
-import {MainMenuComponent} from './main-menu/main-menu.component';
-import {ChatComponent} from './chat/chat.component';
-import {ThreadComponent} from './thread/thread.component';
-import {NewChannelComponent} from './new-channel/new-channel.component';
+import { Component } from '@angular/core';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ChatComponent } from './chat/chat.component';
+import { ThreadComponent } from './thread/thread.component';
+import { NewChannelComponent } from './new-channel/new-channel.component';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-workspace',
     imports: [
         MainMenuComponent,
         ChatComponent,
-        ThreadComponent,
+        // ThreadComponent,
         NewChannelComponent,
     ],
     templateUrl: './workspace.component.html',
-    styleUrl: './workspace.component.scss',
+    styleUrls: ['./workspace.component.scss'],
 })
 export class WorkspaceComponent {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    logOut(): void {
+        this.authService.logOut()
+            .then(() => {
+                this.router.navigate(['/']);
+            })
+            .catch((error) => {
+                console.error('Logout failed:', error);
+            });
+    }
 }
