13b18951abdaa78b654f8dd1e152ae2f30541324
Seb887 <seb.dev@posteo.de>
2025-05-13 22:17:44 +0200

    rework main menu html and css

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,35 +1,35 @@
-import { Component } from '@angular/core';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { ChatComponent } from './chat/chat.component';
-import { ThreadComponent } from './thread/thread.component';
-import { NewChannelComponent } from './new-channel/new-channel.component';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
-import { WorkspaceHeaderComponent } from './workspace-header/workspace-header.component';
+import { Component } from "@angular/core";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { ChatComponent } from "./chat/chat.component";
+import { ThreadComponent } from "./thread/thread.component";
+import { NewChannelComponent } from "./new-channel/new-channel.component";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
+import { WorkspaceHeaderComponent } from "./workspace-header/workspace-header.component";
 
 @Component({
-    selector: 'app-workspace',
+    selector: "app-workspace",
     imports: [
         MainMenuComponent,
         ChatComponent,
         // ThreadComponent,
         NewChannelComponent,
-        WorkspaceHeaderComponent
+        WorkspaceHeaderComponent,
     ],
-    templateUrl: './workspace.component.html',
-    styleUrls: ['./workspace.component.scss'],
+    templateUrl: "./workspace.component.html",
+    styleUrls: ["./workspace.component.scss"],
 })
 export class WorkspaceComponent {
-
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router) {}
 
     logOut(): void {
-        this.authService.logOut()
+        this.authService
+            .logOut()
             .then(() => {
-                this.router.navigate(['/']);
+                this.router.navigate(["/"]);
             })
             .catch((error) => {
-                console.error('Logout failed:', error);
+                console.error("Logout failed:", error);
             });
     }
 }
