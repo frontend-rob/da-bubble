1b9b6fa7cfa3a6d4e52d63acd0f4f5a955287834
Seb887 <seb.dev@posteo.de>
2025-05-18 16:52:08 +0200

    add thread service for open and close thread

--- src/app/services/thread.service.ts
+++ src/app/services/thread.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from "@angular/core";
     providedIn: "root",
 })
 export class ThreadService {
-    constructor() {}
-
     private _isThreadOpen = false;
 
     toggleThread(value: boolean) {
