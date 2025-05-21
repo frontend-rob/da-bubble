8c491bb4c857c71399908030b540908839e695bb
majorsoldier <majorsoldier603@proton.me>
2025-05-21 04:21:44 +0200

    add fn call service chennal seletion works now

---
+++ src/app/services/function-trigger.service.spec.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+
+import {FunctionTriggerService} from './function-trigger.service';
+
+describe('FunctionTriggerService', () => {
+    let service: FunctionTriggerService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(FunctionTriggerService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
