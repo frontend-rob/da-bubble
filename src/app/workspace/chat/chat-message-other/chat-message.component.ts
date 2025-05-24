af570b65242843f4eb5b3f60b5d72b31c741ddd9
Seb887 <seb.dev@posteo.de>
2025-05-24 10:24:44 +0200

    add chat options menu

--- src/app/workspace/chat/chat-message-other/chat-message.component.ts
+++ src/app/workspace/chat/chat-message-other/chat-message.component.ts
@@ -1,24 +1,31 @@
-import {Component, Input, OnInit} from "@angular/core";
-import {Message} from "../../../interfaces/message.interface";
-import {ChatService} from "../../../services/chat.service";
+import { Component, Input, OnInit } from "@angular/core";
+import { Message } from "../../../interfaces/message.interface";
+import { ChatService } from "../../../services/chat.service";
+import { ChatOptionBarComponent } from "../chat-option-bar/chat-option-bar.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-chat-message-other",
-    imports: [],
+    imports: [CommonModule, ChatOptionBarComponent],
     templateUrl: "./chat-message.component.html",
     styleUrl: "./chat-message.component.scss",
 })
 export class ChatMessageComponent implements OnInit {
     @Input() message!: Message;
+    isHovered = false;
 
-    constructor(private chatService: ChatService) {
-    }
+    constructor(private chatService: ChatService) {}
 
     ngOnInit(): void {
         console.log(this.message);
+        console.log(this.isHovered);
     }
 
-    toggleThread() {
+    openThread() {
         this.chatService.toggleThread(true);
+    }
+
+    toggleHovered(bool: boolean) {
+        this.isHovered = bool;
     }
 }
