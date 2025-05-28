04b7f17a0204f34ef0ba266f8b909ad2bebaddfe
Seb887 <seb.dev@posteo.de>
2025-05-28 17:37:56 +0200

    add user menu - finished

--- src/app/workspace/workspace-header/workspace-header.component.ts
+++ src/app/workspace/workspace-header/workspace-header.component.ts
@@ -1,23 +1,37 @@
-import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import {
+    Component,
+    EventEmitter,
+    inject,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Subscription } from "rxjs";
 import { UserData } from "../../interfaces/user.interface";
 import { UserService } from "../../services/user.service";
-import { ChatService } from "../../services/chat.service";
+import { ProfileCardComponent } from "../profile-card/profile-card.component";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-workspace-header",
-    imports: [CommonModule],
+    imports: [CommonModule, ProfileCardComponent],
     templateUrl: "./workspace-header.component.html",
     styleUrl: "./workspace-header.component.scss",
 })
 export class WorkspaceHeaderComponent implements OnInit, OnDestroy {
-    isMenuOpen = false;
+    @Input() openMenu!: boolean;
+    @Output() toggleMenu = new EventEmitter<boolean>();
+
+    isUserMenuOpen = false;
+    isProfileCardOpen = false;
     currentUser!: UserData;
     userSubscription!: Subscription;
     private userService: UserService = inject(UserService);
 
-    constructor(public chatService: ChatService) {}
+    constructor(private router: Router, private authService: AuthService) {}
 
     ngOnInit() {
         this.userSubscription = this.userService.currentUser$.subscribe(
@@ -35,11 +49,29 @@ export class WorkspaceHeaderComponent implements OnInit, OnDestroy {
         }
     }
 
-    toggleMenu(bool: boolean) {
-        this.isMenuOpen = bool;
+    openUserMenu() {
+        this.isUserMenuOpen = true;
+        this.toggleMenu.emit(true);
+
+        if (!this.openMenu) {
+            this.isProfileCardOpen = false;
+        }
     }
 
-    toggleProfileMenu(bool: boolean) {
-        this.chatService.toggleProfileMenu(bool);
+    openProfileCard() {
+        this.isProfileCardOpen = true;
+    }
+
+    logOut(): void {
+        this.toggleMenu.emit(false);
+
+        this.authService
+            .logOut()
+            .then(() => {
+                this.router.navigate(["/"]);
+            })
+            .catch((error) => {
+                console.error("Logout failed:", error);
+            });
     }
 }
--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
 import { MainMenuComponent } from "./main-menu/main-menu.component";
 import { ChatComponent } from "./chat/chat.component";
-import { AuthService } from "../services/auth.service";
-import { Router } from "@angular/router";
 import { WorkspaceHeaderComponent } from "./workspace-header/workspace-header.component";
 import { ThreadComponent } from "./thread/thread.component";
 import { CommonModule } from "@angular/common";
@@ -25,11 +23,9 @@ import { ProfileCardComponent } from "./profile-card/profile-card.component";
     providers: [ChatService],
 })
 export class WorkspaceComponent {
-    constructor(
-        private authService: AuthService,
-        private router: Router,
-        public chatService: ChatService
-    ) {}
+    isProfileMenuOpen = false;
+
+    constructor(public chatService: ChatService) {}
 
     get isThreadOpen() {
         return this.chatService.isThreadOpen;
@@ -39,34 +35,19 @@ export class WorkspaceComponent {
         return this.chatService.isProfileInfoOpen;
     }
 
-    get isProfileMenuOpen() {
-        return this.chatService.isProfileInfoOpen;
-    }
-
-    logOut(): void {
-        this.authService
-            .logOut()
-            .then(() => {
-                this.router.navigate(["/"]);
-            })
-            .catch((error) => {
-                console.error("Logout failed:", error);
-            });
-    }
-
     toggleProfileInfo(bool: boolean) {
         this.chatService.toggleProfileInfo(bool);
     }
 
-    toggleProfileMenu(bool: boolean) {
-        this.chatService.toggleProfileMenu(bool);
+    handleProfileMenu(bool: boolean) {
+        this.isProfileMenuOpen = bool;
     }
 
     close() {
         if (this.isProfileInfoOpen) {
             this.chatService.toggleProfileInfo(false);
         } else if (this.isProfileMenuOpen) {
-            this.chatService.toggleProfileMenu(false);
+            this.isProfileMenuOpen = false;
         }
     }
 }
