e3aad06d804696dac9b4e0072a16b48123b79856
majorSoldier603 <96193790+majorSoldier603@users.noreply.github.com>
2025-06-09 23:51:00 +0200

    refactor: update user status and role handling to use boolean values and adjust related templates and logic

--- src/app/onboarding/avatars/avatars.component.ts
+++ src/app/onboarding/avatars/avatars.component.ts
@@ -139,8 +139,8 @@ export class AvatarsComponent {
             email: userData.email,
             photoURL: this.selectedUserAvatar,
             createdAt: Timestamp.fromDate(new Date()),
-            status: 'offline',
-            role: 'user'
+            status: true,
+            role: {user: true, admin: false, guest: false, moderator: false}
         };
         await this.authService.saveUserToFirestore(uid, user);
     }
