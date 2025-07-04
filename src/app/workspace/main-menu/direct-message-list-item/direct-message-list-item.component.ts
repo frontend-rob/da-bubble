d48dbf2dff9512e7ca0bd6340081cd5a45d84763
majorSoldier603 <96193790+majorSoldier603@users.noreply.github.com>
2025-07-04 09:13:18 +0200

    Add DirectMessageListItem component with UI and active chat handling

---
+++ src/app/workspace/main-menu/direct-message-list-item/direct-message-list-item.component.ts
@@ -0,0 +1,36 @@
+import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {Component, Input} from "@angular/core";
+import {UserData} from "../../../interfaces/user.interface";
+import {ChatService} from "../../../services/chat.service";
+
+@Component({
+	selector: "app-direct-message-list-item",
+	imports: [CommonModule, NgOptimizedImage],
+	templateUrl: "./direct-message-list-item.component.html",
+	styleUrl: "./direct-message-list-item.component.scss",
+})
+export class DirectMessageListItemComponent {
+	@Input() chatPartner!: UserData;
+
+	allUsers: UserData[] = [];
+	availableUsersForDM: UserData[] = [];
+
+	constructor(
+		private chatService: ChatService // private functionTriggerService: FunctionTriggerService, // private userService: UserService
+	) {
+	}
+
+	get isActive(): boolean {
+		// console.log(this.chatPartner);
+		if (this.chatPartner && this.chatPartner.uid) {
+			return this.chatPartner.uid === this.chatService.activeChat;
+		} else {
+			return false;
+		}
+	}
+
+	setActiveChat(id: string) {
+		this.chatService.setActiveChat(id);
+		this.chatService.handleNewMessage(false);
+	}
+}
